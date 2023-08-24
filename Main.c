dcade011afa3040985016c0c870e68774d1271d7
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-08-24 05:18:21 +0300

    done

---
+++ Main.c
@@ -0,0 +1,78 @@
+#include "shell.h"
+
+void active()
+{
+    char *prompt = "($) ";
+    char *line, *path, *temp;
+    char **words;
+    int st = -1, shift = 0;
+
+    head_d = build_dirs();
+    while (1)
+    {
+        printf("%s", prompt);
+        line = Read();
+        shift = strspn(line, cmd_DELIM);
+        words = Parse(line, cmd_DELIM);
+
+        if (words != NULL)
+        {
+            if(strchr(words[0], '/'))           
+                st = Execute(words);
+            else if ((path = find_path(head_d, words[0])))
+            {
+                temp = words[0];
+                words[0] = path;
+                free(temp - shift);
+                shift = 0;
+                st = Execute(words);
+            }
+            else
+                perror("Not Found--->");
+
+            free(words[0] - shift);
+            free(words);
+        }
+        free(line);
+
+        if (st >= 0)
+            exit(st);
+    }
+}
+
+void lazy()
+{
+    char *line;
+    char **words;
+    int st = -1, shift = 0;
+
+    while (1)
+    {
+        line = Read();
+        shift = strspn(line, cmd_DELIM);
+        words = Parse(line, cmd_DELIM);
+
+        if (words != NULL)
+        {
+            st = Execute(words);
+            free(words[0] - shift);
+            free(words);
+        }
+        free(line);
+
+        if (st >= 0)
+        {
+            exit(st);
+        }
+    }
+}
+
+int main(void)
+{
+        if (isatty(STDIN_FILENO))
+                active();
+        else
+                lazy();
+
+        return (0);
+}
