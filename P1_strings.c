c638e8c6d1ebff2ce78e6bc97de1f54f4cbd7c33
MaryomaElsayed <maryamelsayd.1@gmail.com>
2023-08-25 17:30:59 -0700

    ueb

--- P1_strings.c
+++ P1_strings.c
@@ -1,5 +1,10 @@
 #include "shell.h"
 
+/**my_strlen - a function wich returns the length
+ * of a string
+ * return : returns the value of length of a string
+ * @m : the pointer of the string
+ */
 size_t my_strlen(const char *m)
 {
 	size_t ln = 0;
@@ -13,6 +18,17 @@ size_t my_strlen(const char *m)
 	return ln;
 }
 
+/**
+ * my_atrchr : it is a function to locate the first
+ * occurence of a specifice character in a string
+ * @str : the pointrr for the inpute string
+ * return : pointer to the first occurrence of a
+ * character in the inpute string
+ * or NULL if the character is not found in
+ * the string
+ * @ch : it is the character to be occurrend
+ */
+ *
 char* my_strchr(const char* str, int ch)
 {
 	while (*str != '\0')
@@ -25,6 +41,15 @@ char* my_strchr(const char* str, int ch)
 	return NULL;
 }
 
+/**
+ * my_strcpy : It is a function to copy a string 
+ * pointied to as source to a buffer pointed to
+ * as destination
+ * @src : pointer for the source
+ * @dst : pointer to the destination
+ *
+ * Return : pointer to the destination
+ */
 char *my_strcpy(char *dst, const char *src)
 {
 	int m = 0;
